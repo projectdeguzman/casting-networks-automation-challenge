3852fa049abe89f9b37822d7c3ef042c47d97096
projectdeguzman <56742422+projectdeguzman@users.noreply.github.com>
2023-02-05 16:01:32 -0700

    Update checkout.cy.js
    updated docs

--- cypress/pages/checkout.cy.js
+++ cypress/pages/checkout.cy.js
@@ -5,12 +5,19 @@ export class Checkout {
 
     /**
      * Navigate the the checkout page.
-     * @returns 
+     * @returns {Cypress.Chainable} - Chainable object representing the DOM element matching the selector
      */
     navigate() {
         return cy.get('#sc-buy-box-ptc-button').click();
     }
 
+     /**
+     * Verify the expected item count and subtotal in the checkout page
+     * @function
+     * @param {number} expectedCount - The expected number of items in the order
+     * @param {number} subtotal - The expected subtotal of the order
+     * @return {Cypress.Chainable} - Chainable object representing the DOM element matching the selector
+     */
     verifyCheckout(expectedCount, subtotal) {
         return cy.get('#spc-order-summary #subtotals-marketplace-table tbody tr:nth-child(1)')
             .should('include.text', `Items (${expectedCount})`) // Verify Item Count Label
